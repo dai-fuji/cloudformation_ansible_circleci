5bcfa406c6303c2d17d821cfd350bfcbbf9ef74a
fujimoto <dai.fuji.110728@gmail.com>
2022-04-10 08:47:01 +0900

    add servespec

---
+++ serverspec/spec/spec_helper.rb
@@ -0,0 +1,48 @@
+require 'serverspec'
+require 'net/ssh'
+require 'yaml'
+
+properties = YAML.load_file('properties.yml')
+
+host = ENV['TARGET_HOST']
+set_property properties[host]
+
+set :backend, :ssh
+
+if ENV['ASK_SUDO_PASSWORD']
+  begin
+    require 'highline/import'
+  rescue LoadError
+    fail "highline is not available. Try installing it."
+  end
+  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+else
+  set :sudo_password, ENV['SUDO_PASSWORD']
+end
+
+host = ENV['TARGET_HOST']
+
+options = Net::SSH::Config.for(host)
+
+options[:user] ||= Etc.getlogin
+
+# add ssh options
+unless properties[host][:ssh_opts].nil?
+  options.merge!(properties[host][:ssh_opts])
+end
+
+# add pty option
+set :request_pty, true
+
+set :host,        options[:host_name] || host
+set :ssh_options, options
+
+# Disable sudo
+# set :disable_sudo, true
+
+
+# Set environment variables
+# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+
+# Set PATH
+# set :path, '/sbin:/usr/local/sbin:$PATH'
